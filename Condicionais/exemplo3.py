222401eda66ca10c1f613f1bfe1d7ae01c89f54f
Ana Beatriz <biaibc23@gmail.com>
2024-06-18 22:48:51 -0300

    Atividades

---
+++ Condicionais/exemplo3.py
@@ -0,0 +1,10 @@
+temperatura = 30
+if temperatura > 25:
+    print("Vale a pena, a temperatura está agradavel")
+elif temperatura < 20:
+    print("Está frio, bora codar!")
+elif temperatura < 10:
+    print("Coberto")
+else:
+    print("Está bem, bora codar! ")
+
---
+++ Condicionais/if.py
@@ -0,0 +1,4 @@
+idade = 30
+if idade >=18:
+    print("Você é maior de idade! ")
+    
